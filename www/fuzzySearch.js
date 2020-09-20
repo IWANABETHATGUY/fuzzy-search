f8f6e05ae251afbe447c2aa559a238f7ea6f0bb9
IWANABETHATGUY <974153916@qq.com>
2020-09-20 22:01:04 +0800

    feat: 🎸 finish demo

--- www/fuzzySearch.js
+++ www/fuzzySearch.js
@@ -3,7 +3,8 @@
  * @param {string[]} patternList 
  * @param {string} source 
  */
-function fuzzysearch(patternList, source) {
+export function fuzzysearch(patternList, source) {
+  source = source.toLowerCase()
   let sourceLen = source.length;
   let patternLen = patternList.reduce((pre, cur) => { return pre + cur.length }, 0)
   if (patternLen > sourceLen) {
@@ -25,29 +26,50 @@ function fuzzysearch(patternList, source) {
       return { match: false };
     }
   }
-  return { match: true, indexList };
+  return { match: true, indexList: merge(indexList,), index: indexList, name: source };
 }
 
-matchEqual(fuzzysearch(['car', 'eel'], 'cartwheel'), true);
-matchEqual(fuzzysearch(['cwhl'], 'cartwheel'), true);
-matchEqual(fuzzysearch(['cwheel'], 'cartwheel'), true);
-matchEqual(fuzzysearch(['cartwheel'], 'cartwheel'), true);
+// matchEqual(fuzzysearch(['car', 'eel'], 'cartwheel'), true);
+// matchEqual(fuzzysearch(['cwhl'], 'cartwheel'), true);
+// matchEqual(fuzzysearch(['cwheel'], 'cartwheel'), true);
+// matchEqual(fuzzysearch(['cartwheel'], 'cartwheel'), true);
 
-matchEqual(fuzzysearch(['cwheeel'], 'cartwheel'), false);
-matchEqual(fuzzysearch(['lw'], 'cartwheel'), false);
-matchEqual(fuzzysearch(['c', 'a', 'r', 't', 'w', 'h', 'e', 'e', 'l'], 'cartwheel'), true);
+// matchEqual(fuzzysearch(['cwheeel'], 'cartwheel'), false);
+// matchEqual(fuzzysearch(['lw'], 'cartwheel'), false);
+// matchEqual(fuzzysearch(['c', 'a', 'r', 't', 'w', 'h', 'e', 'e', 'l'], 'cartwheel'), true);
 
-// chinese unicode testcase
-matchEqual(fuzzysearch(['语言'], 'php语言'), true);
-matchEqual(fuzzysearch(['hp语'], 'php语言'), true);
-matchEqual(fuzzysearch(['Py开发'], 'Python开发者'), true);
-matchEqual(fuzzysearch(['Py 开发'], 'Python开发者'), false);
-matchEqual(fuzzysearch(['爪哇进阶'], '爪哇开发进阶'), true);
-matchEqual(fuzzysearch(['格式工具'], '非常简单的格式化工具'), true);
-matchEqual(fuzzysearch(['正则'], '学习正则表达式怎么学习'), true);
-matchEqual(fuzzysearch(['学习正则'], '正则表达式怎么学习'), false);
+// // chinese unicode testcase
+// matchEqual(fuzzysearch(['语言'], 'php语言'), true);
+// matchEqual(fuzzysearch(['hp语'], 'php语言'), true);
+// matchEqual(fuzzysearch(['Py开发'], 'Python开发者'), true);
+// matchEqual(fuzzysearch(['Py 开发'], 'Python开发者'), false);
+// matchEqual(fuzzysearch(['爪哇进阶'], '爪哇开发进阶'), true);
+// matchEqual(fuzzysearch(['格式工具'], '非常简单的格式化工具'), true);
+// matchEqual(fuzzysearch(['正则'], '学习正则表达式怎么学习'), true);
+// matchEqual(fuzzysearch(['学习正则'], '正则表达式怎么学习'), false);
 
 function matchEqual(a, b) {
   console.log(a);
   console.assert(a.match === b);
 }
+
+function merge(indexList) {
+  let start = 0;
+  let end = 0;
+  const res = []
+  while (end < indexList.length) {
+    if (start === end || indexList[end] - indexList[end - 1] === 1) {
+      end += 1;
+    } else {
+      res.push({ start: indexList[start], end: indexList[end - 1] });
+      start = end;
+    }
+  }
+  if (end === indexList.length) {
+    res.push({ start: indexList[start], end: indexList[end - 1] });
+  }
+  return res;
+}
+const res = fuzzysearch(['ss'], 'lewiss sstracke iv')
+console.log(res);
+// console.log(merge([1, 3, 5, 7, 8, 10]));
--- www/index.js
+++ www/index.js
@@ -1,3 +1,34 @@
-import * as wasm from "hello-wasm-pack";
+// import * as wasm from "hello-wasm-pack";
 
-wasm.greet();
+// wasm.greet();
+import { fuzzysearch } from './fuzzySearch'
+import faker from 'faker'
+
+const input = document.getElementById('input')
+const result = document.getElementById('result')
+const nameList = []
+input.addEventListener('input', e => {
+    console.time('search')
+    const searchList = e.target.value.toLowerCase().split(/\s+/);
+    const res = nameList.map(item => ({ ...fuzzysearch(searchList, item) })).filter(item => item.match)
+    console.timeEnd('search');
+    const html = res.map(item => {
+        let h = []
+        let last = 0
+        for (let i = 0; i < item.indexList.length; i++) {
+            const {start, end} = item.indexList[i];
+            h.push(item.name.slice(last, start))
+            h.push(`<span class="highlight">${item.name.slice(start, end + 1)}</span>`)
+            last = end + 1
+        }
+        h.push(item.name.slice(last, item.name.length))
+        return `<li>${h.join('')}|${item.index}|${JSON.stringify(item.indexList)}</li>`
+    }).join('')
+
+    result.innerHTML = html
+})
+const set = new Set();
+for (let i = 0; i < 10000; i++) {
+    set.add(faker.name.findName())
+}
+nameList.push(...set)
