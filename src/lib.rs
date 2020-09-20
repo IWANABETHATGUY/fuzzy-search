b4ef55faf4e9b261b99e7e15cf4c7527d74da204
IWANABETHATGUY <974153916@qq.com>
2020-09-21 00:49:55 +0800

    feat: 🎸 init wasm fuzzy-search

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,71 @@
 mod utils;
 
+use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
-
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+use web_sys::console;
 
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+pub struct Timer<'a> {
+    name: &'a str,
 }
 
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, fuzzy-search!");
+impl<'a> Timer<'a> {
+    pub fn new(name: &'a str) -> Timer<'a> {
+        console::time_with_label(name);
+        Timer { name }
+    }
 }
+
+impl<'a> Drop for Timer<'a> {
+    fn drop(&mut self) {
+        console::time_end_with_label(self.name);
+    }
+}
+#[wasm_bindgen]
+#[derive(Serialize, Deserialize)]
+pub struct Test {
+    a: usize,
+    b: Vec<String>,
+    source: String
+}
+#[wasm_bindgen]
+pub fn fuzzy_search(pattern_list: &JsValue, source: &str) -> JsValue {
+    // Timer::new("wasm-search");
+    let mut vec: Vec<String> = pattern_list.into_serde().unwrap();
+    JsValue::from_serde(&Test {
+        a: vec.len() as usize,
+        b: vec,
+        source: source.into(),
+    })
+    .unwrap()
+}
+
+// export function fuzzysearch(patternList, source) {
+//   source = source.toLowerCase()
+//   let sourceLen = source.length;
+//   let patternLen = patternList.reduce((pre, cur) => { return pre + cur.length }, 0)
+//   if (patternLen > sourceLen) {
+//     return false;
+//   }
+//   const patternListLength = patternList.length;
+//   const indexList = []
+//   first: for (let i = 0, j = 0; i < patternListLength; i++) {
+//     let pattern = patternList[i];
+//     second: for (let c = 0; c < pattern.length; c++) {
+//       let nch = pattern.charCodeAt(c);
+//       while (j < sourceLen) {
+//         if (source.charCodeAt(j) === nch) {
+//           indexList.push(j++);
+//           continue second;
+//         }
+//         j++;
+//       }
+//       return { match: false };
+//     }
+//   }
+//   return { match: true, indexList: merge(indexList,), index: indexList, name: source };
+// }
--- www/index.js
+++ www/index.js
@@ -2,16 +2,24 @@
 
 // wasm.greet();
 import { fuzzysearch } from './fuzzySearch'
+import * as wasm from 'fuzzy-search'
+
 import faker from 'faker'
 
 const input = document.getElementById('input')
 const result = document.getElementById('result')
+
 const nameList = []
+const res = wasm.fuzzy_search(["fuck", 'test'], "fuck");
+console.log(res);
 input.addEventListener('input', e => {
     console.time('search')
     const searchList = e.target.value.toLowerCase().split(/\s+/);
     const res = nameList.map(item => ({ ...fuzzysearch(searchList, item) })).filter(item => item.match)
     console.timeEnd('search');
+    console.time('search-wasm');
+    const res2 = nameList.map(item => ({ ...wasm.fuzzy_search(searchList, item) })).filter(item => item.match)
+    console.timeEnd('search-wasm');
     const html = res.map(item => {
         let h = []
         let last = 0
@@ -31,4 +39,6 @@ const set = new Set();
 for (let i = 0; i < 10000; i++) {
     set.add(faker.name.findName())
 }
+set.add('简爱')
+set.add('简单的爱')
 nameList.push(...set)
